acf5dccc05b40f87d97fde84847aae864d613716
zenin1504 <kayozzprojects@yandex.com>
2026-09-17 16:51:33 +0000

    duchamp: Include Turbo charging and Thermal tiles
    Signed-off-by: Adarsh0127-Elite <adarshmoharana027@gmail.com>

--- parts/src/com/xiaomi/settings/BootCompletedReceiver.kt
+++ parts/src/com/xiaomi/settings/BootCompletedReceiver.kt
@@ -14,6 +14,7 @@ import android.util.Log
 import android.view.Display;
 import android.view.Display.HdrCapabilities;
 import com.xiaomi.settings.display.ColorService
+import com.xiaomi.settings.turbocharging.TurboChargingService
 
 class BootCompletedReceiver : BroadcastReceiver() {
 
@@ -43,6 +44,10 @@ class BootCompletedReceiver : BroadcastReceiver() {
     private fun onLockedBootCompleted(context: Context) {
         // Display
         ColorService.startService(context)
+
+        // Start TurboChargingService
+        val turboChargingIntent = Intent(context, TurboChargingService::class.java)
+        context.startService(turboChargingIntent)
     }
 
     companion object {
