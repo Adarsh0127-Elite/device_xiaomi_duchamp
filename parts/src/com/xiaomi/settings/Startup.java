850d9c9d478799c08a16fb7f28390108f9e1e7bd
kamikaonashi <thorschi93@gmx.at>
2026-09-17 17:04:10 +0000

    duchamp: parts: Add core control
    Introduce userspace interface for CPU core hotplug management,
    allowing individual cores to be brought online/offline dynamically.
    Maintains minimum 2-core (little) requirement to ensure system responsiveness
    
    Signed-off-by: Adarsh0127-Elite <adarshmoharana027@gmail.com>

---
+++ parts/src/com/xiaomi/settings/Startup.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2025 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.xiaomi.settings;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class Startup extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+    }
+}
